774d734e9db13836ed3471d54fdd8e8468401d59
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-01-03 15:34:24 -0700

    Initial Commit

---
+++ MovieBuilder/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MovieBuilder
+//
+//  Created by Yoshiki Vázquez Baeza on 1/3/14.
+//  Copyright (c) 2014 Knight Laboratory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
