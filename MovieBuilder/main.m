af4009033238f1b3cc518802961d568b8712b8ad
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-01-03 18:02:17 -0700

    First working version

--- MovieBuilder/main.m
+++ MovieBuilder/main.m
@@ -1,22 +1,179 @@
 //
 //  main.m
-//  MovieBuilder
+//  mbu
 //
 //  Created by Yoshiki Vázquez Baeza on 1/3/14.
 //  Copyright (c) 2014 Knight Laboratory. All rights reserved.
 //
+//  Based on python code by Jens Uwe Nockel, http://pages.uoregon.edu/noeckel/
+//  see this post: http://mathematica.stackexchange.com/a/4903/1509
+//
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#import <QTKit/QTKit.h>
+
+void CreateMOVFileFromFilenames(NSArray *filenames, NSArray *durations,
+								NSString *outputFilename);
+
 
 int main(int argc, const char * argv[])
 {
 
 	@autoreleasepool {
-	    
-	    // insert code here...
-	    NSLog(@"Hello, World!");
-	    
+		NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
+
+		NSError *error=nil;
+
+		NSString *filepathForListOfFilenames = nil;
+		NSString *outputFilepath = nil;
+
+		filepathForListOfFilenames = [args stringForKey:@"i"] ? [args stringForKey:@"i"] : [args stringForKey:@"-input"];
+		outputFilepath = [args stringForKey:@"o"] ? [args stringForKey:@"o"] : [args stringForKey:@"-output"];
+
+		// ghetto way to check if help is on or not
+		if ( [[[NSProcessInfo processInfo] arguments] containsObject:@"-h"] ||
+			[[[NSProcessInfo processInfo] arguments] containsObject:@"--help"]){
+			fprintf(stdout, "mbu: movie builder\n");
+			fprintf(stdout, "-i (--input) - input filepath to list of frames "\
+							"and durations\n");
+			fprintf(stdout, "-o (--output) - output filepath to the name of "\
+							"the movie\n");
+			fprintf(stdout, "-h (--help) - help text\n");
+			exit(0);
+		}
+
+		if (outputFilepath == nil) {
+			outputFilepath = @"out.mov";
+		}
+		if (filepathForListOfFilenames == nil) {
+			NSLog(@"The -i (--input) argument is required, pass in a list of "
+				  "filepaths");
+			exit(11);
+		}
+
+		error = nil;
+		NSString *contents = [NSString stringWithContentsOfFile:filepathForListOfFilenames
+													   encoding:NSUTF8StringEncoding
+														  error:&error];
+
+		if (error) {
+			NSLog(@"Error reading contents of file: %@",
+				  [error localizedDescription]);
+		}
+
+		// separate on new lines
+		NSMutableArray *arrayOfLines = (NSMutableArray *)[contents componentsSeparatedByCharactersInSet:
+														  [NSCharacterSet newlineCharacterSet]];
+		[arrayOfLines removeObjectAtIndex:[arrayOfLines count]-1];
+		NSMutableArray *arrayOfDurations = nil, *arrayOfFilenames = nil;
+
+		if ([[[arrayOfLines objectAtIndex:0] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] count] > 1) {
+			arrayOfFilenames = [[NSMutableArray alloc] init];
+			arrayOfDurations = [[NSMutableArray alloc] init];
+
+			NSArray *buffer = nil;
+
+			for (NSString *string in arrayOfLines) {
+				buffer = [string componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+
+				[arrayOfFilenames addObject:[buffer objectAtIndex:0]];
+				[arrayOfDurations addObject:[buffer objectAtIndex:1]];
+
+				// worthwile checking that all filepaths exist
+				if (![[NSFileManager defaultManager]
+					  fileExistsAtPath:[buffer objectAtIndex:0]]) {
+					NSLog(@"Error, file %@ does not exist!",
+						  [buffer objectAtIndex:0]);
+					exit(12);
+				}
+			}
+		}
+		else{
+			arrayOfFilenames = [[NSMutableArray alloc] initWithArray:arrayOfLines];
+			arrayOfDurations = [[NSMutableArray alloc] init];
+
+			for (NSUInteger index = 0; index<[arrayOfFilenames count]; index++) {
+				[arrayOfDurations addObject:@"30"];
+			}
+		}
+		NSLog(@"%ld frames were loaded", [arrayOfFilenames count]);
+
+		CreateMOVFileFromFilenames((NSArray *)arrayOfFilenames,
+								   (NSArray *)arrayOfDurations, outputFilepath);
+
 	}
     return 0;
 }
+
+void CreateMOVFileFromFilenames(NSArray *filenames, NSArray *durations,
+								NSString *outputFilename)
+{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+	NSError *error = nil;
+	NSImage *image = nil;
+	QTTime frameDuration;
+
+	NSNumber *someNumber = [NSNumber numberWithLong:codecHighQuality];
+	NSDictionary *attributes = @{QTAddImageCodecType: @"avc1",
+								 QTAddImageCodecQuality: someNumber};
+
+	QTMovie *movieBuilder = [[QTMovie alloc] initToWritableFile:outputFilename
+														  error:&error];
+
+	if (error) {
+		NSLog(@"Error ocurred when creating QTMovie: %@",
+			   [error localizedDescription]);
+		return;
+	}
+
+	for(int index=0; index < [filenames count]; index++){
+		image = [[NSImage alloc]
+				 initWithContentsOfFile:[filenames objectAtIndex:index]];
+
+		frameDuration = QTMakeTime([[durations objectAtIndex:index] longLongValue],
+								   600);
+
+		[movieBuilder addImage:image
+				   forDuration:frameDuration
+				withAttributes:attributes];
+		[movieBuilder updateMovieFile];
+	}
+#pragma clang diagnostic pop
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
