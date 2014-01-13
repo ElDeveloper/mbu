d280fba11fa53c935cdce3e199f9664e38eb23bd
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-01-12 22:17:35 -0700

    Tabs -> spaces
    nuff said

--- MovieBuilder/main.m
+++ MovieBuilder/main.m
@@ -14,143 +14,146 @@
 #import <QTKit/QTKit.h>
 
 void CreateMOVFileFromFilenames(NSArray *filenames, NSArray *durations,
-								NSString *outputFilename);
+                                NSString *outputFilename);
 
 
 int main(int argc, const char * argv[])
 {
 
-	@autoreleasepool {
-		NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
+    @autoreleasepool {
+        NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
 
-		NSError *error=nil;
+        NSError *error=nil;
 
-		NSString *filepathForListOfFilenames = nil;
-		NSString *outputFilepath = nil;
+        NSString *filepathForListOfFilenames = nil;
+        NSString *outputFilepath = nil;
 
-		filepathForListOfFilenames = [args stringForKey:@"i"] ? [args stringForKey:@"i"] : [args stringForKey:@"-input"];
-		outputFilepath = [args stringForKey:@"o"] ? [args stringForKey:@"o"] : [args stringForKey:@"-output"];
+        filepathForListOfFilenames = [args stringForKey:@"i"] ? [args stringForKey:@"i"] : [args stringForKey:@"-input"];
+        outputFilepath = [args stringForKey:@"o"] ? [args stringForKey:@"o"] : [args stringForKey:@"-output"];
 
-		// ghetto way to check if help is on or not
-		if ([[[NSProcessInfo processInfo] arguments] containsObject:@"-h"] ||
-			[[[NSProcessInfo processInfo] arguments] containsObject:@"--help"]){
-			fprintf(stdout, "mbu: movie builder\n");
-			fprintf(stdout, "-i (--input) - input filepath to list of frames "\
-							"and durations\n");
-			fprintf(stdout, "-o (--output) - output filepath to the name of "\
-							"the movie\n");
-			fprintf(stdout, "-h (--help) - help text\n");
-			exit(0);
-		}
+        // ghetto way to check if help is on or not
+        if ([[[NSProcessInfo processInfo] arguments] containsObject:@"-h"] ||
+            [[[NSProcessInfo processInfo] arguments] containsObject:@"--help"]){
+            fprintf(stdout, "mbu: movie builder\n");
+            fprintf(stdout, "-i (--input) - input filepath to list of frames "\
+                            "and durations\n");
+            fprintf(stdout, "-o (--output) - output filepath to the name of "\
+                            "the movie\n");
+            fprintf(stdout, "-h (--help) - help text\n");
+            exit(0);
+        }
 
-		// set default filename to out.mov
-		if (outputFilepath == nil) {
-			outputFilepath = @"out.mov";
-		}
+        // set default filename to out.mov
+        if (outputFilepath == nil) {
+            outputFilepath = @"out.mov";
+        }
 
-		// do not execute a thing if the file input is not passed in
-		if (filepathForListOfFilenames == nil) {
-			fprintf(stdout, "The -i (--input) argument is required, pass in a "
-					"list of filepaths\n");
-			exit(11);
-		}
+        // do not execute a thing if the file input is not passed in
+        if (filepathForListOfFilenames == nil) {
+            fprintf(stdout, "The -i (--input) argument is required, pass in a "
+                    "list of filepaths\n");
+            exit(11);
+        }
 
-		error = nil;
-		NSString *contents = [NSString stringWithContentsOfFile:filepathForListOfFilenames
-													   encoding:NSUTF8StringEncoding
-														  error:&error];
+        error = nil;
+        NSString *contents = [NSString stringWithContentsOfFile:filepathForListOfFilenames
+                                                       encoding:NSUTF8StringEncoding
+                                                          error:&error];
 
-		if (error) {
-			fprintf(stdout, "Error reading contents of file: %s\n",
-					[[error localizedDescription] UTF8String]);
-			exit(12);
-		}
+        if (error) {
+            fprintf(stdout, "Error reading contents of file: %s\n",
+                    [[error localizedDescription] UTF8String]);
+            exit(12);
+        }
 
-		// separate on new lines
-		NSMutableArray *arrayOfLines = (NSMutableArray *)[contents componentsSeparatedByCharactersInSet:
-														  [NSCharacterSet newlineCharacterSet]];
-		[arrayOfLines removeObjectAtIndex:[arrayOfLines count]-1];
-		NSMutableArray *arrayOfDurations = nil, *arrayOfFilenames = nil;
+        // separate on new lines
+        NSMutableArray *arrayOfLines = (NSMutableArray *)[contents componentsSeparatedByCharactersInSet:
+                                                          [NSCharacterSet newlineCharacterSet]];
+        [arrayOfLines removeObjectAtIndex:[arrayOfLines count]-1];
+        NSMutableArray *arrayOfDurations = nil, *arrayOfFilenames = nil;
 
-		// check if there are durations in the contents of the file
-		if ([[[arrayOfLines objectAtIndex:0] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] count] > 1) {
-			arrayOfFilenames = [[NSMutableArray alloc] init];
-			arrayOfDurations = [[NSMutableArray alloc] init];
+        // check if there are durations in the contents of the file
+        if ([[[arrayOfLines objectAtIndex:0] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] count] > 1) {
+            arrayOfFilenames = [[NSMutableArray alloc] init];
+            arrayOfDurations = [[NSMutableArray alloc] init];
 
-			NSArray *buffer = nil;
+            NSArray *buffer = nil;
 
-			// retrieve the durations and save them in the arrays
-			for (NSString *string in arrayOfLines) {
-				buffer = [string componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            // retrieve the durations and save them in the arrays
+            for (NSString *string in arrayOfLines) {
+                buffer = [string componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
 
-				[arrayOfFilenames addObject:[buffer objectAtIndex:0]];
-				[arrayOfDurations addObject:[buffer objectAtIndex:1]];
+                [arrayOfFilenames addObject:[buffer objectAtIndex:0]];
+                [arrayOfDurations addObject:[buffer objectAtIndex:1]];
 
-				// worthwile checking that all filepaths exist
-				if (![[NSFileManager defaultManager]
-					  fileExistsAtPath:[buffer objectAtIndex:0]]) {
-					fprintf(stdout, "Error, file %s does not exist!\n",
-						  [[buffer objectAtIndex:0] UTF8String]);
-					exit(12);
-				}
-			}
-		}
-		// if there is not a duration in the file then just set this to 30
-		else{
-			arrayOfFilenames = [[NSMutableArray alloc] initWithArray:arrayOfLines];
-			arrayOfDurations = [[NSMutableArray alloc] init];
+                // worthwile checking that all filepaths exist
+                if (![[NSFileManager defaultManager]
+                      fileExistsAtPath:[buffer objectAtIndex:0]]) {
+                    fprintf(stdout, "Error, file %s does not exist!\n",
+                          [[buffer objectAtIndex:0] UTF8String]);
+                    exit(12);
+                }
+            }
+        }
+        // if there is not a duration in the file then just set this to 30
+        else{
+            arrayOfFilenames = [[NSMutableArray alloc] initWithArray:arrayOfLines];
+            arrayOfDurations = [[NSMutableArray alloc] init];
 
-			for (NSUInteger index = 0; index<[arrayOfFilenames count]; index++) {
-				[arrayOfDurations addObject:@"30"];
-			}
-		}
-		fprintf(stdout, "%ld frames were loaded\n", [arrayOfFilenames count]);
+            for (NSUInteger index = 0; index<[arrayOfFilenames count]; index++) {
+                [arrayOfDurations addObject:@"30"];
+            }
+        }
+        fprintf(stdout, "%ld frames were loaded\n", [arrayOfFilenames count]);
 
-		// create the movie
-		CreateMOVFileFromFilenames((NSArray *)arrayOfFilenames,
-								   (NSArray *)arrayOfDurations, outputFilepath);
+        // create the movie
+        CreateMOVFileFromFilenames((NSArray *)arrayOfFilenames,
+                                   (NSArray *)arrayOfDurations, outputFilepath);
 
-	}
+    }
     return 0;
 }
 
 void CreateMOVFileFromFilenames(NSArray *filenames, NSArray *durations,
-								NSString *outputFilename)
+                                NSString *outputFilename)
 {
 // for some reason Apple has deprecated most of what's in the QTKit as of 10.9
 // this nifty trick was taken from http://stackoverflow.com/a/14065939/379593
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-	NSError *error = nil;
-	NSImage *image = nil;
-	QTTime frameDuration;
+    NSError *error = nil;
+    NSImage *image = nil;
+    QTTime frameDuration;
 
-	NSNumber *codecQuality = [NSNumber numberWithLong:codecHighQuality];
-	NSDictionary *attributes = @{QTAddImageCodecType: @"avc1",
-								 QTAddImageCodecQuality: codecQuality};
+    NSNumber *codecQuality = [NSNumber numberWithLong:codecHighQuality];
+    NSDictionary *attributes = @{QTAddImageCodecType: @"avc1",
+                                 QTAddImageCodecQuality: codecQuality};
 
-	QTMovie *movieBuilder = [[QTMovie alloc] initToWritableFile:outputFilename
-														  error:&error];
+    QTMovie *movieBuilder = [[QTMovie alloc] initToWritableFile:outputFilename
+                                                          error:&error];
 
-	if (error) {
-		fprintf(stdout, "Error ocurred when creating QTMovie: %s\n",
-			   [[error localizedDescription] UTF8String]);
-		return;
-	}
+    if (error) {
+        fprintf(stdout, "Error ocurred when creating QTMovie: %s\n",
+               [[error localizedDescription] UTF8String]);
+        return;
+    }
 
-	for(int index=0; index < [filenames count]; index++){
-		image = [[NSImage alloc]
-				 initWithContentsOfFile:[filenames objectAtIndex:index]];
+    for(int index=0; index < [filenames count]; index++){
+        image = [[NSImage alloc]
+                 initWithContentsOfFile:[filenames objectAtIndex:index]];
 
-		frameDuration = QTMakeTime([[durations objectAtIndex:index] longLongValue],
-								   600);
+        frameDuration = QTMakeTime([[durations objectAtIndex:index] longLongValue],
+                                   600);
 
-		[movieBuilder addImage:image
-				   forDuration:frameDuration
-				withAttributes:attributes];
-	}
-	[movieBuilder updateMovieFile];
+        [movieBuilder addImage:image
+                   forDuration:frameDuration
+                withAttributes:attributes];
+#ifdef DEBUG
+        fprintf(stdout, "Added %d\n", index);
+#endif
+    }
+    [movieBuilder updateMovieFile];
 #pragma clang diagnostic pop
 }
 
